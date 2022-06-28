13838834327604788e7e8230ded26ecc4129f3e9
Adnan Abrar <83346033+adnanabrar786@users.noreply.github.com>
2022-06-28 01:43:10 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-// import logo from './logo.svg';
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import Navbar from "./component/Navbar/Navbar";
 import Talkexpert from "./component/Talkexpert/Talkexpert";
 import ConsultingServices from "./component/ConsultingServices/ConsultingServices";
 import Technologis from "./component/Technologies/Technologis";
-import Footer from "./component/Footer/Footer";
 import LatestWork from "./component/LatestWork/LatestWork";
 import ClientSay from "./component/ClientSay.jsx/ClientSay";
 
